ce7aa0866c7a63d81eebb91a0749dbbf0453ab21
KDanila <concista@gmail.com>
2020-04-21 17:23:33 +0300

    Spring thymeleaf bootstrap project

---
+++ src/main/java/ru/kdv/model/Employee.java
@@ -0,0 +1,18 @@
+package ru.kdv.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+}
+
+
+
